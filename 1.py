e996960dc9a91a4089821585b4cf74c798d2d85d
Ishwor Khanal <35824645+ishworkhanal900@users.noreply.github.com>
2019-04-01 12:01:52 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+# Python Program - Calculate Perimeter of Rectangle
+
+length = float(input("Enter length of rectangle: "));
+breadth = float(input("Enter breadth of rectangle: "));
+perimeter = (2*length) + (2*breadth);
+area = length*breadth;
+print("\nArea of Rectangle =", round(area,4));
+print("\nPerimeter of Rectangle =", round(perimeter,4));
+    
